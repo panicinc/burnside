3679a5620045fc12456776317fb9ff05d1f241a0
James Moore <james@panic.com>
2012-09-18 12:08:48 -0700

    removed an errant !

--- bin/tweet.rb
+++ bin/tweet.rb
@@ -85,7 +85,7 @@ end
 
 # Capture the status id of the tweet we're replying to
 reply_status_regex = /^<(\d+)@.*>/
-if !(mail[:in_reply_to].decoded !=~ reply_status_regex)
+if !(mail[:in_reply_to].decoded =~ reply_status_regex)
 	$stderr.puts "The In-Reply-To header isn't in the correct format"
 	exit(1)
 end
